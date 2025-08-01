03f3df35021cf58bdd62b8c115c515bcb7ef1745
v1ctorkn <mwendavictor42@gmail.com>
2025-08-01 13:19:40 +0300

    new calculator

--- calc.py
+++ calc.py
@@ -3,18 +3,28 @@ first_number = float(input("Enter first number: "))
 second_number = float(input("Enter second number: "))
 
 # the code below will add the two numbers
-print(first_number + second_number)
+result_sum = first_number + second_number
+print("Sum:", result_sum)
+
 # the code below will subtract the two numbers
-print(first_number - second_number)
+result_subtraction = first_number - second_number
+print("Subtraction:", result_subtraction) 
 # the code below will multiply the two numbers
-print(first_number * second_number)
+result_multiplication = first_number * second_number
+print("Multiplication:", result_multiplication)
+
 # the code below will divide the two numbers
-print(first_number / second_number)
+result_division = first_number / second_number
+print("Division:", result_division)
+
 # the code below will raise first_number to the power of second_number
-print(first_number ** second_number)
+print("Power:", first_number ** second_number)
+
 # the code below will perform floor division
-print(first_number // second_number)
+print("Floor Division:", first_number // second_number)
+
 # the code below will find the modulus
-print(first_number % second_number)
+print("Modulus:", first_number % second_number)
+
 # the code below will perform bitwise AND (only works with integers)
-print(int(first_number) & int(second_number))
+print("Bitwise AND:", int(first_number) & int(second_number))
