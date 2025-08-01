11d7a3a06846b402b9a2fd244aaa5da5761a72a3
v1ctorkn <mwendavictor42@gmail.com>
2025-08-01 13:11:14 +0300

    m

---
+++ calc.py
@@ -0,0 +1,20 @@
+# basic calculator code in python 
+first_number = float(input("Enter first number: "))
+second_number = float(input("Enter second number: "))
+
+# the code below will add the two numbers
+print(first_number + second_number)
+# the code below will subtract the two numbers
+print(first_number - second_number)
+# the code below will multiply the two numbers
+print(first_number * second_number)
+# the code below will divide the two numbers
+print(first_number / second_number)
+# the code below will raise first_number to the power of second_number
+print(first_number ** second_number)
+# the code below will perform floor division
+print(first_number // second_number)
+# the code below will find the modulus
+print(first_number % second_number)
+# the code below will perform bitwise AND (only works with integers)
+print(int(first_number) & int(second_number))
